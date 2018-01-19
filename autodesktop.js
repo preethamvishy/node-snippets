08d9ca9aa7d631558d4a33d9ef5b6883fdd096a9
Preetham Viswanathan <preethamvishy10@gmail.com>
2018-01-20 03:51:24 +0530

    Desktop automation using RobotJS

---
+++ autodesktop.js
@@ -0,0 +1,17 @@
+// Thanks to https://github.com/octalmage/robotjs
+// Snippet coutesy of their documentation
+var robot = require("robotjs");
+robot.setMouseDelay(2);
+
+var twoPI = Math.PI * 2.0;
+var screenSize = robot.getScreenSize();
+var height = (screenSize.height / 2) - 10;
+var width = screenSize.width;
+
+for (var x = 0; x < width; x++)
+{
+	y = height * Math.sin((twoPI * x) / width) + height;
+	robot.moveMouse(x, y);
+}
+
+console.log(robot.getMousePos());
